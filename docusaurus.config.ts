e35d0fb1ec606c4559c8a17db4d1880f61d8b975
Bastien Dumont <bastienxs@gmail.com>
2024-05-12 17:23:10 +0200

    fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://bastiendmt.github.io/quality-code/',
+  url: 'https://bastiendmt.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/quality-code/',
