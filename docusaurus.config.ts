fc9bf19b1ce38d761c16d644433683599af0c54f
Bastien Dumont <bastienxs@gmail.com>
2024-05-12 17:21:15 +0200

    update baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://bastiendmt.github.io/quality-code/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/quality-code/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
